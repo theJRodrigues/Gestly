bd90717cbcdd8972ab6b8945b21f1e85c7b026b0
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-03-19 23:10:06 -0300

    feat: add customer routes to use CreateCustomerController and CreateCustomerRepository

--- backend/src/routes/CustomerRoutes.ts
+++ backend/src/routes/CustomerRoutes.ts
@@ -1,11 +1,15 @@
 import express, { Request, Response } from "express";
 import Customer from "../models/Customer";
+import { CreateCustomerRepository } from "../repositories/customers-repositories/create-customer-mongo/CreateCustomerRepository";
+import { CreateCustomerController } from "../controllers/customers-controllers/create-customer/CreateCustomerController";
 const customerRoutes = express.Router();
 
-customerRoutes.post("/customer", async (req, res) => {
-const customer = req.body;
-  const newCustomer = await Customer.create(customer);
-  res.status(201).json(newCustomer);
+customerRoutes.post("/create", async (req, res) => {
+  const createCustomerRepository = new CreateCustomerRepository()
+  const createCustomerController = new CreateCustomerController(createCustomerRepository);
+
+  const {statusCode, body} = await createCustomerController.handleCreateCustomer(req.body)
+  res.status(statusCode).json(body)
 });
 
 export default customerRoutes;

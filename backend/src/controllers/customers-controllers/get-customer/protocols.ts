204b820d00ae5231f98d775cd5030391c06d792d
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-03-20 22:28:33 -0300

    feat: add protocols for GetAllCustomersController and GetAllCustomersRepository

---
+++ backend/src/controllers/customers-controllers/get-customer/protocols.ts
@@ -0,0 +1,10 @@
+import { ICustomer } from "../../../models/Customer";
+import { IHTTPResponse } from "../../protocols";
+
+export interface IGetAllCustomersController{
+    handleGetAllCustomers(): Promise<IHTTPResponse<ICustomer[]>>
+}
+
+export interface IGetAllCustomersRepository{
+    getAllCustomers(): Promise<ICustomer[]>
+}

21f1df64f87583933ffac188342d7df63a419475
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-03-21 00:28:17 -0300

    feat: update exports in controllers and repositories

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,2 +1,3 @@
-export * from "./customers-controllers/create-customer/CreateCustomerController"
-export * from "./customers-controllers/get-customer/GetAllCustomers/GetAllCustomersController"
+export * from "./customers-controllers/create-customer/CreateCustomerController";
+export * from "./customers-controllers/get-customers/get-all-customers/GetAllCustomersController";
+export * from "./customers-controllers/get-customers/get-customer-by-id/GetCustomerByIdController";
--- backend/src/repositories/index.ts
+++ backend/src/repositories/index.ts
@@ -1,2 +1,3 @@
 export * from './customers-repositories/create-customer-mongo/CreateCustomerRepository'
 export * from './customers-repositories/get-customer-mongo/GetAllCustomersRepository'
+export * from './customers-repositories/get-customer-mongo/GetCustomerByIdRepository'

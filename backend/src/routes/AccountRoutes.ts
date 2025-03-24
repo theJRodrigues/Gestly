32e329097527ea30255417e88778b39cef3b5c86
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-03-24 20:11:49 -0300

    refactor: simplify account registration route by using CreateAccountController directly

--- backend/src/routes/AccountRoutes.ts
+++ backend/src/routes/AccountRoutes.ts
@@ -1,15 +1,10 @@
 import express from "express";
-import { CreateAccountRepository } from "../repositories/accounts/create/CreateAccountRepository";
 import { CreateAccountController } from "../controllers/accounts/create/CreateAccountController";
 
 const accountsRoutes = express.Router();
-accountsRoutes.post("/register", async (req, res) => {
-  const createAccountsRepository = new CreateAccountRepository();
-  const createAccountsController = new CreateAccountController(
-    createAccountsRepository
-  );
-  const { statusCode, body } =
-    await createAccountsController.handleCreateAccount(req.body);
-  res.status(statusCode).json(body);
+
+accountsRoutes.post("/register",(req, res) => {
+  const createAccount = new CreateAccountController()
+  createAccount.create(res, req.body)
 });
 export default accountsRoutes;

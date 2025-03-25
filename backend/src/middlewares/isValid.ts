b142a54770cfa3b428a965462fab474bfb13b79c
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-03-24 22:22:26 -0300

    refactor: improve validation error handling in isValid middleware

--- backend/src/middlewares/isValid.ts
+++ backend/src/middlewares/isValid.ts
@@ -2,15 +2,21 @@ import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator";
 import { statusCode } from "../controllers/protocols";
 
-export class isValidy{
-    static validate(req: Request, res: Response, next: NextFunction): void{
-        const errors = validationResult(req)
+export class isValidy {
+  static validationErrors(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): void {
+    const errors = validationResult(req);
 
-        if(!errors.isEmpty()){
-           res.status(statusCode.UnprocessableEntity).json({errors: errors.array()})
-           return
-        }
-
-        next()
+    if (!errors.isEmpty()) {
+      res
+        .status(statusCode.UnprocessableEntity)
+        .json({ errors: errors.array() });
+      return;
     }
+
+    next();
+  }
 }

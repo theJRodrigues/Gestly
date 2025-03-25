2fbf4eb3a237368cb9112726a8ff7c4d3fc71ff8
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-03-24 22:23:16 -0300

    refactor: rename validation method and streamline validation logic for account fields

--- backend/src/middlewares/ValidateAccountInfos.ts
+++ backend/src/middlewares/ValidateAccountInfos.ts
@@ -3,23 +3,26 @@ import { body, ValidationChain } from "express-validator";
 class CreateAccountValidate {
   private static validateFirstname(): ValidationChain {
     return body("firstname")
-        .exists().withMessage("O primeiro nome é obrigatório")
-        .bail()
-        .trim().notEmpty().withMessage("O primeiro nome não pode ser vazio")
+      .exists()
+      .withMessage("O primeiro nome é obrigatório")
+      .bail()
+      .trim()
+      .notEmpty()
+      .withMessage("O primeiro nome não pode ser vazio");
   }
 
   private static validateLastname(): ValidationChain {
     return body("lastname")
-        .exists().withMessage("O último nome é obrigatório")
-        .bail()
-        .trim().notEmpty().withMessage("O último nome não pode ser vazio")
+      .exists()
+      .withMessage("O último nome é obrigatório")
+      .bail()
+      .trim()
+      .notEmpty()
+      .withMessage("O último nome não pode ser vazio");
   }
 
-  static validate(): ValidationChain[] {
-    return [
-        this.validateFirstname(), 
-        this.validateLastname()
-    ];
+  static validateFields(): ValidationChain[] {
+    return [this.validateFirstname(), this.validateLastname()];
   }
 }
 

e85a2c2dc2e2b738be0d58ea005f9637866e475a
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-03-26 00:21:07 -0300

    refactor: rename create method in CreateAccountRepository for clarity

--- backend/src/repositories/accounts/create/CreateAccountRepository.ts
+++ backend/src/repositories/accounts/create/CreateAccountRepository.ts
@@ -1,12 +1,11 @@
-import {
-  ICreateAccountRepository,
-  AccountWithoutId,
-} from "../../../controllers/accounts/create/protocols";
+import { AccountWithoutId, ICreateAccountRepository } from "../../../controllers";
 import Account, { IAccount } from "../../../models/Account";
 
+
+
 export class CreateAccountRepository implements ICreateAccountRepository {
-  async create(account: AccountWithoutId): Promise<IAccount> {
-    const createAccount = await Account.create(account);
-    return createAccount;
+  async createAccount(account: AccountWithoutId): Promise<IAccount> {
+    const newAccount = await Account.create(account);
+    return newAccount;
   }
 }

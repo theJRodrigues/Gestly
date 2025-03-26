ba0fdca96db45c8a9f818ac8a81d0141ca6cff76
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-03-26 19:43:12 -0300

    feat: add validation for account with email method

--- backend/src/controllers/protocols.ts
+++ backend/src/controllers/protocols.ts
@@ -5,6 +5,7 @@ export enum statusCode {
   Unauthorized = 401,
   Forbidden = 403,
   NotFound = 404,
+  Conflict = 409,
   UnprocessableEntity = 422,
   InternalServerError = 500,
   ServiceUnavailable = 503,

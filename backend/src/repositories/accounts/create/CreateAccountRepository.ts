b48929f670110945e048b288bd7d2c3ff2c6591d
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-03-26 19:43:43 -0300

    feat: add method to validate existing account by email in CreateAccountRepository

--- backend/src/repositories/accounts/create/CreateAccountRepository.ts
+++ backend/src/repositories/accounts/create/CreateAccountRepository.ts
@@ -4,6 +4,12 @@ import Account, { IAccount } from "../../../models/Account";
 
 
 export class CreateAccountRepository implements ICreateAccountRepository {
+
+  async validateExistingAccountWithEmail(account: AccountWithoutId): Promise<boolean>{
+    const existingAccount= await Account.findOne({email: account.email})
+    return !!existingAccount
+  }
+
   async createAccount(account: AccountWithoutId): Promise<IAccount> {
     const newAccount = await Account.create(account);
     return newAccount;

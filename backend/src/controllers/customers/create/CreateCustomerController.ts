4a1a9435c4bb487006acd985644a59dd175f5a30
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-03-26 00:21:40 -0300

    refactor: implement createCustomer method in CreateCustomerController with improved error handling

--- backend/src/controllers/customers/create/CreateCustomerController.ts
+++ backend/src/controllers/customers/create/CreateCustomerController.ts
@@ -1,18 +1,33 @@
-import { Response } from "express";
+import { Request, Response } from "express";
 import { ICustomer } from "../../../models/Customer";
 import { statusCode } from "../../protocols";
-import { ICreateCustomerController, ICreateCustomerRepository, TCustomer } from "./protocols";
+import {
+  ICreateCustomerController,
+  ICreateCustomerRepository,
+  CustomerWithoutId,
+} from "./protocols";
 
-// export class CreateCustomerController implements ICreateCustomerController{
-//     constructor(private readonly createCustomerRepository: ICreateCustomerRepository) {}   
-    
-//     async handleCreateCustomer(customer: TCustomer): Promise<Response<ICustomer>> {
-//         try {
-//           const createCustomer = await this.createCustomerRepository.createCustomer(customer)
-//           return {statusCode: statusCode.Created, body: createCustomer}  
-//         } catch (error) {
-//             return {statusCode: statusCode.BadRequest, body: "Ocorreu um erro" + error}
-//         }
-//     }
+export class CreateCustomerController implements ICreateCustomerController {
+  constructor(
+    private readonly createCustomerRepository: ICreateCustomerRepository
+  ) {}
 
-// }
+  async createCustomer(
+    req: Request,
+    res: Response
+  ): Promise<Response<ICustomer>> {
+    try {
+      const customer: CustomerWithoutId = req.body;
+      const newCustomer = await this.createCustomerRepository.createCustomer(
+        customer
+      );
+
+      return res.status(statusCode.Created).json(newCustomer);
+    } catch (error) {
+      return res.status(statusCode.BadRequest).json({
+        errorDB:
+          "Ocorreu um erro ao tentar criar o usuário no banco de dados " + error,
+      });
+    }
+  }
+}

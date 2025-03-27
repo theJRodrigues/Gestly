14ec0023034b2dfd2bf4d88641a0a7705e89b133
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-03-26 21:31:57 -0300

    feat: add validation methods for existing customers by CPF and email in ICreateCustomerRepository interface

--- backend/src/controllers/customers/create/protocols.ts
+++ backend/src/controllers/customers/create/protocols.ts
@@ -3,9 +3,18 @@ import { ICustomer } from "../../../models/Customer";
 export type CustomerWithoutId = Omit<ICustomer, "id">;
 
 export interface ICreateCustomerController {
-  createCustomer(req: Request, res: Response): Promise<Response<ICustomer>>;
+  createCustomer(
+    req: Request,
+    res: Response
+  ): Promise<Response<ICustomer | string>>;
 }
 
 export interface ICreateCustomerRepository {
+  validateExistingCustomerWithCPF(
+    customer: CustomerWithoutId
+  ): Promise<boolean>;
+
+  validateExistingWithEmail(customer: CustomerWithoutId): Promise<boolean>;
+
   createCustomer(customer: CustomerWithoutId): Promise<ICustomer>;
 }

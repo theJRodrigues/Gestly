858992b598e92de06c7b5d5eabba07e493284099
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-03-26 21:30:43 -0300

    feat: add validation methods to check for existing customers by CPF and email in CreateCustomerRepository

--- backend/src/repositories/customers/create/CreateCustomerRepository.ts
+++ backend/src/repositories/customers/create/CreateCustomerRepository.ts
@@ -5,6 +5,20 @@ import {
 import Customer, { ICustomer } from "../../../models/Customer";
 
 export class CreateCustomerRepository implements ICreateCustomerRepository {
+  async validateExistingCustomerWithCPF(
+    customer: CustomerWithoutId
+  ): Promise<boolean> {
+    const isCreatedByCPF = await Customer.findOne({ cpf: customer.cpf });
+    return !!isCreatedByCPF;
+  }
+
+  async validateExistingWithEmail(
+    customer: CustomerWithoutId
+  ): Promise<boolean> {
+    const isCreatedByEmail = await Customer.findOne({ email: customer.email });
+    return !!isCreatedByEmail;
+  }
+
   async createCustomer(customer: CustomerWithoutId): Promise<ICustomer> {
     const createCustomer = await Customer.create(customer);
     return createCustomer;

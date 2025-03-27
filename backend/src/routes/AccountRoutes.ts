f52a7adbc3a307db61c1ffc9c63ccbd56eb9e93e
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-03-27 19:47:25 -0300

    refactor: replace CreateAccountValidate with ValidationCreateAccount in account routes

--- backend/src/routes/AccountRoutes.ts
+++ backend/src/routes/AccountRoutes.ts
@@ -1,21 +1,20 @@
 import express, { Request, Response } from "express";
 import { CreateAccountController } from "../controllers/accounts/create/CreateAccountController";
-import CreateAccountValidate from "../middlewares/CreateAccountValidate";
+import ValidationCreateAccount from "../middlewares/validations-account/create/ValidationCreateAccount";
 import { isValidy } from "../middlewares/isValid";
 import { CreateAccountRepository } from "../repositories";
 
 const accountsRoutes = express.Router();
 
-accountsRoutes.post("/register",
-  CreateAccountValidate.validateFields(),
+accountsRoutes.post(
+  "/register",
+  ValidationCreateAccount.validateFields(),
   isValidy.validationErrors,
   (req: Request, res: Response) => {
-    const createAccountRepository = 
-    new CreateAccountRepository()
-    
-    const createAccount = 
-    new CreateAccountController(createAccountRepository);
-    
+    const createAccountRepository = new CreateAccountRepository();
+
+    const createAccount = new CreateAccountController(createAccountRepository);
+
     createAccount.createAccount(req, res);
   }
 );

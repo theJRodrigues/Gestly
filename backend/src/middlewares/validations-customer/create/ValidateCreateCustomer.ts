e900b6525101976e65e3eef5f0eae8d48bf07ed8
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-03-29 20:27:42 -0300

    feat: add more validations in middleware ValidationCreateCustomer

---
+++ backend/src/middlewares/validations-customer/create/ValidateCreateCustomer.ts
@@ -0,0 +1,172 @@
+import { body, ValidationChain } from "express-validator";
+
+export class ValidateCreateCustomer {
+  private static validateFirstName(): ValidationChain {
+    return body("firstname")
+      .exists()
+      .withMessage("O primeiro nome é obrigatório.")
+      .bail()
+      .notEmpty({ ignore_whitespace: true })
+      .withMessage("O primeiro nome não pode estar vazio.")
+      .bail();
+  }
+  private static validateLastName(): ValidationChain {
+    return body("lastname")
+      .exists()
+      .withMessage("O sobrenome é obrigatório.")
+      .bail()
+      .notEmpty({ ignore_whitespace: true })
+      .withMessage("O sobrenome não pode estar vazio.")
+      .bail();
+  }
+  private static validateEmail(): ValidationChain {
+    return body("email")
+      .exists()
+      .withMessage("O email é obrigatório.")
+      .bail()
+      .isEmail()
+      .withMessage("O email deve ser válido.")
+      .bail();
+  }
+
+  private static validateContact(): ValidationChain {
+    return body("contact")
+      .exists()
+      .withMessage("O contato é obrigatório.")
+      .bail();
+  }
+  private static validateContactDDD(): ValidationChain {
+    return body("contact.DDD")
+      .exists()
+      .withMessage("O DDD é obrigatório.")
+      .bail()
+      .trim()
+      .isLength({ min: 2, max: 2 })
+      .withMessage("DDD informado está incorreto.")
+      .bail()
+      .toInt()
+      .custom((value) => {
+        if (value < 11 || value > 99) {
+          throw new Error(
+            "O DDD informado não existe. O DDD deve ser entre 11 a 99."
+          );
+        }
+        return true;
+      })
+      .bail();
+  }
+
+  private static validateContactNumber(): ValidationChain {
+    return body("contact.number")
+      .exists()
+      .withMessage("O número do contato é obrigatório.")
+      .bail()
+      .trim()
+      .isLength({ min: 9, max: 9 })
+      .withMessage("O número de contato informado deve conter 9 números.")
+      .bail();
+  }
+
+  private static validateCPF(): ValidationChain {
+    return body("cpf")
+      .exists()
+      .withMessage("O CPF é obrigatório.")
+      .bail()
+      .trim()
+      .isLength({ min: 11, max: 11 })
+      .withMessage("O CPF deve conter 11 números.")
+      .bail();
+  }
+  private static validateBirthDay(): ValidationChain {
+    return body("birthDate")
+      .exists()
+      .withMessage("A data de nascimento é obrigatória")
+      .bail()
+      .isDate()
+      .withMessage("A data de nascimento está no formato incorreto.")
+      .bail()
+      .toDate()
+      .custom((birthDate: Date) => {
+        const date = birthDate.toLocaleDateString("pt-BR", { timeZone: "UTC" });
+        const dateNow = new Date().toLocaleDateString("pt-BR", {
+          timeZone: "UTC",
+        });
+        if (date >= dateNow) {
+          throw new Error(
+            "A data de nascimento não pode ser maior ou igual a data atual."
+          );
+        }
+        return true;
+      })
+      .bail();
+  }
+
+  private static validateAddress(): ValidationChain {
+    return body("address")
+      .exists()
+      .withMessage("O endereço é obrigatório")
+      .bail();
+  }
+
+  private static validateAdrressCep(): ValidationChain {
+    return body("address.cep")
+      .exists()
+      .withMessage("O CEP é obrigatório.")
+      .bail()
+      .trim()
+      .isLength({ min: 8, max: 8 })
+      .withMessage("O CEP deve conter 8 números.")
+      .bail();
+  }
+
+  private static validateAddressStreet(): ValidationChain {
+    return body("address.street")
+      .exists()
+      .withMessage("O nome da Rua é obrigatório.")
+      .bail()
+      .notEmpty({ ignore_whitespace: true })
+      .withMessage("A campo Rua não pode estar vazio")
+      .bail();
+  }
+  private static validateAddressNumber(): ValidationChain {
+    return body("address.number")
+      .exists()
+      .withMessage("O número do endereço é obrigatório.")
+      .bail()
+      .notEmpty({ ignore_whitespace: true })
+      .withMessage("O campo número do endereço não pode estar vazio.")
+      .bail();
+  }
+  private static validateAddressComplement(): ValidationChain {
+    return body("address.complement")
+      .optional()
+      .notEmpty({ ignore_whitespace: true })
+      .withMessage("O campo complemento não pode estar vazio")
+      .bail();
+  }
+  private static validateAddressRefPoint(): ValidationChain {
+    return body("address.refPoint")
+      .optional()
+      .notEmpty({ ignore_whitespace: true })
+      .withMessage("O ponto de referência não pode estar vazio.");
+  }
+
+  static validateFields(): ValidationChain[] {
+    return [
+      this.validateFirstName(),
+      this.validateLastName(),
+      this.validateEmail(),
+      this.validateContact(),
+      this.validateContactDDD(),
+      this.validateContactNumber(),
+      this.validateCPF(),
+      this.validateBirthDay(),
+      this.validateAddress(),
+      this.validateAdrressCep(),
+      this.validateAddressStreet(),
+      this.validateAddressNumber(),
+      this.validateAddressComplement(),
+      this.validateAddressRefPoint(),
+    ];
+  }
+}

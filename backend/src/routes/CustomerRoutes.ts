3acdf706b2735a628c5da4efbc337092eadc8e3c
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-03-29 20:30:29 -0300

    refactor: streamline customer routes by improving import statements and adding validation middleware

--- backend/src/routes/CustomerRoutes.ts
+++ backend/src/routes/CustomerRoutes.ts
@@ -1,12 +1,8 @@
-import express from "express";
-import {
-  CreateCustomerController,
-  GetCustomerByIdController,
-} from "../controllers";
-import {
-  CreateCustomerRepository,
-  GetCustomerByIdRepository,
-} from "../repositories";
+import express, { Request, Response } from "express";
+import {CreateCustomerController,GetCustomerByIdController} from "../controllers";
+import {CreateCustomerRepository,GetCustomerByIdRepository} from "../repositories";
+import { ValidateCreateCustomer } from "../middlewares/validations-customer/create/ValidateCreateCustomer";
+import { isValidy } from "../middlewares/isValid";
 const customerRoutes = express.Router();
 
 // customerRoutes.get("/", async (_req, res) => {
@@ -21,23 +17,25 @@ const customerRoutes = express.Router();
 // });
 
 customerRoutes.get("/:id", (req, res) => {
-  const getCustomerByIdRepository = 
-  new GetCustomerByIdRepository();
+  const getCustomerByIdRepository = new GetCustomerByIdRepository();
 
-  const getCustomerByIdController = 
-  new GetCustomerByIdController(getCustomerByIdRepository);
+  const getCustomerByIdController = new GetCustomerByIdController(
+    getCustomerByIdRepository
+  );
 
   getCustomerByIdController.handleGetCustomerById(req, res);
 });
 
-customerRoutes.post("/create", async (req, res) => {
-  const createCustomerRepository = 
-  new CreateCustomerRepository()
+customerRoutes.post("/create",
+  ValidateCreateCustomer.validateFields(),
+  isValidy.validationErrors,
+  (req: Request, res: Response) => {
+    const createCustomerRepository = new CreateCustomerRepository();
 
-  const createCustomerController = 
-  new CreateCustomerController(createCustomerRepository);
-  
-  createCustomerController.createCustomer(req, res)
- });
+    const createCustomerController = new CreateCustomerController(createCustomerRepository);
+
+    createCustomerController.createCustomer(req, res);
+  }
+);
 
 export default customerRoutes;

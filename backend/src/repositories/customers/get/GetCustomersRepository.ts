5edc265b7f20c7e6d9db7828d0d9ba3907a5e046
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-03-30 16:06:44 -0300

    refactor:  renamed archives

--- backend/src/repositories/customers/get/GetCustomersRepository.ts
+++
@@ -1,9 +0,0 @@
-import { IGetCustomersRepository } from "../../../controllers/customers/get/get-all/protocols";
-import Customer, { ICustomer } from "../../../models/Customer";
-
-export class GetCustomersRepository implements IGetCustomersRepository {
-  async getAllCustomers(): Promise<ICustomer[]> {
-    const getAllCustomers = await Customer.find();
-    return getAllCustomers;
-  }
-}

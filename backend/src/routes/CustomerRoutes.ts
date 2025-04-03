4310dadf1093732a39ce0c2ead718d585faa708b
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-04-02 21:07:24 -0300

    refactor: refactored to follow clean architeture and solid.

--- backend/src/routes/CustomerRoutes.ts
+++ backend/src/routes/CustomerRoutes.ts
@@ -1,38 +1,30 @@
 import express, { Request, Response } from "express";
-import {
-  CreateCustomerController,
-  GetCustomerByIdController,
-  GetAllCustomersController,
-} from "@controllers";
-import {
-  CreateCustomerRepository,
-  GetAllCustomersRepository,
-  GetCustomerByIdRepository,
-} from "@repositories";
 import { ValidateCreateCustomer, isValidy } from "@middlewares";
+import { CreateCustomerFactory } from "factories/customer/CreateCustomer.factory";
+import { CreateCustomerAdapter } from "adapters/express/customer/CreateCustomer.adapter";
 
 const customerRoutes = express.Router();
 
-customerRoutes.get("/", (_req, res) => {
-  const repository = new GetAllCustomersRepository();
-  const controller = new GetAllCustomersController(repository);
-  controller.getAll(res);
-});
+// customerRoutes.get("/", (_req, res) => {
+//   const repository = new GetAllCustomersRepository();
+//   const controller = new GetAllCustomersController(repository);
+//   controller.getAll(res);
+// });
 
-customerRoutes.get("/:id", (req, res) => {
-  const repository = new GetCustomerByIdRepository();
-  const controller = new GetCustomerByIdController(repository);
-  controller.get(req, res);
-});
+// customerRoutes.get("/:id", (req, res) => {
+//   const repository = new GetCustomerByIdRepository();
+//   const controller = new GetCustomerByIdController(repository);
+//   controller.get(req, res);
+// });
 
-customerRoutes.post(
-  "/create",
+customerRoutes.post( "/create",
   ValidateCreateCustomer.validateFields(),
   isValidy.validationErrors,
   (req: Request, res: Response) => {
-    const repository = new CreateCustomerRepository();
-    const controller = new CreateCustomerController(repository);
-    controller.createCustomer(req, res);
+    const controller = CreateCustomerFactory.make();
+    const adapter = new CreateCustomerAdapter(controller);
+
+    adapter.handle(req, res);
   }
 );
 

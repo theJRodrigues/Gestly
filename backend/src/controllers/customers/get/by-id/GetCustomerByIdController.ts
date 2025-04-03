300f5c1a31668bdeda5e386dcaf08190c13b8993
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-04-02 21:07:42 -0300

    refactor: comment out unused code in customer controllers and remove CreateAccountFactory

--- backend/src/controllers/customers/get/by-id/GetCustomerByIdController.ts
+++ backend/src/controllers/customers/get/by-id/GetCustomerByIdController.ts
@@ -1,29 +1,29 @@
-import { ICustomer } from "@models";
-import {
-  IGetCustomerByIdController,
-  IGetCustomerByIdRepository,
-  statusCode
-} from "@protocols";
+// import { ICustomer } from "@models";
+// import {
+//   IGetCustomerByIdController,
+//   IGetCustomerByIdRepository,
+//   statusCode
+// } from "@protocols";
 
-export class GetCustomerByIdController implements IGetCustomerByIdController {
-  constructor(private readonly repository: IGetCustomerByIdRepository) {}
+// export class GetCustomerByIdController implements IGetCustomerByIdController {
+//   constructor(private readonly repository: IGetCustomerByIdRepository) {}
 
-  async getById(req: Request, res: Response): Promise<Response<ICustomer>> {
-    try {
-      const { id: customerId } = req.params;
-      const customer = await this.repository.getById(customerId);
+//   async getById(req: Request, res: Response): Promise<Response<ICustomer>> {
+//     try {
+//       const { id: customerId } = req.params;
+//       const customer = await this.repository.getById(customerId);
 
-      if (!customer) {
-        return res.status(statusCode.NotFound).json("Cliente não encontrado");
-      }
+//       if (!customer) {
+//         return res.status(statusCode.NotFound).json("Cliente não encontrado");
+//       }
 
-      return res.status(statusCode.OK).json(customer);
-    } catch (error) {
-      return res
-        .status(statusCode.BadRequest)
-        .json(
-          "Ocorreu um erro solicitar o cliente no banco de dados \n" + error
-        );
-    }
-  }
-}
+//       return res.status(statusCode.OK).json(customer);
+//     } catch (error) {
+//       return res
+//         .status(statusCode.BadRequest)
+//         .json(
+//           "Ocorreu um erro solicitar o cliente no banco de dados \n" + error
+//         );
+//     }
+//   }
+// }

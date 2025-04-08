3370d1a1b7e6bc0560a03e7fdd121418a4206427
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-04-08 19:17:33 -0300

    refactor: update  callback of create customer repository methods to return clearer error messages.

--- backend/src/domains/customer/repositories/createCustomerRepository.protocol.ts
+++ backend/src/domains/customer/repositories/createCustomerRepository.protocol.ts
@@ -1,9 +1,9 @@
 import { CustomerDTO } from "@domains/customer";
 
 export interface ICreateCustomerRepository {
-    findByCPF(cpf: string): Promise<boolean>;
+    findByCPF(cpf: string): Promise<CustomerDTO | null>;
   
-    findByEmail(email: string): Promise<boolean>;
+    findByEmail(email: string): Promise<CustomerDTO | null>;
   
     create(customer: CustomerDTO): Promise<CustomerDTO>;
   }

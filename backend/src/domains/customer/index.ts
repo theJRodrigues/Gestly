aef5ab469f78f21bbf82077190894792da44756b
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-04-07 21:50:19 -0300

    refactor: update imports

---
+++ backend/src/domains/customer/index.ts
@@ -0,0 +1,5 @@
+export * from "./dtos";
+export * from './usecases';
+export * from './repositories';
+export * from './entities';
+export * from './factories';
--- backend/src/shared/services/index.ts
+++ backend/src/shared/services/index.ts
@@ -1,2 +1,1 @@
-export * from "./ValidateUniqueCustomer.service";
 export * from "./GetEnvVaribles.service";

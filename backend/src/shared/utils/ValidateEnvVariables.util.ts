40884b5ec4f991b1d47752f998f68134b28a5e0e
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-04-09 20:08:49 -0300

    refactor: remove dotenv lib imports

--- backend/src/shared/utils/ValidateEnvVariables.util.ts
+++ backend/src/shared/utils/ValidateEnvVariables.util.ts
@@ -1,11 +1,6 @@
 import { envs } from "@shared/constants"
-import dotenv from "dotenv"
-dotenv.config()
-
-
-
 export class ValidateEnvVariables{
-    static async execute(){
+    static execute(){
         for(const [key, value] of Object.entries(envs)){
             if(!value){
                 throw new Error(`A variável de ambiente ${key} não está definida`)

75f30d280a8339d49be403aa002f4526d8a98980
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-04-12 23:05:21 -0300

    refactor: separating validation methods for clarity

--- backend/src/domains/customer/usecases/CreateCustomer.usecase.ts
+++ backend/src/domains/customer/usecases/CreateCustomer.usecase.ts
@@ -10,65 +10,43 @@ import {
   ICreateCustomerRepository,
   Customer,
 } from "@domains/customer";
-import { ValidateAccountIdExistService } from "@shared/services";
+import { IValidateExistAccountService } from "@infrastructure/MongoDB";
 
 export class CreateCustomerUseCase implements ICreateCustomerUseCase {
-  constructor(private readonly repository: ICreateCustomerRepository) {}
-
-  async create(
-    customer: CustomerDTO
-  ): Promise<IHttpResponse<CustomerDTO | IHttpErrorResponse>> {
-    const newCustomer = new Customer(customer);
-
+  constructor(
+    private readonly repository: ICreateCustomerRepository,
+    private readonly validateExistAccountService: IValidateExistAccountService
+  ) {}
+  async create(customerDTO: CustomerDTO)
+  : Promise<IHttpResponse<Customer | IHttpErrorResponse>> {
+    
     try {
-      const isExistAccountId = 
-      await ValidateAccountIdExistService
-      .validate(customer.accountIdRef);
-      if (!isExistAccountId) {
-        return {
-          statusCode: HttpStatusCode.NotFound,
-          body: {
-            error: "Conta não encontrada! Faça o login novamente!",
-          },
-        };
+      const customer = new Customer(customerDTO);
+
+      const isNotExistAccount = await this.validateExistAccount(customer.accountIdRef)
+      if (isNotExistAccount) {
+        return isNotExistAccount;
       }
 
-      const emailAlreadyUsedOnTheCustomer = await this.repository.findByEmail(newCustomer.email );
-      if (emailAlreadyUsedOnTheCustomer) {
-        return {
-          statusCode: HttpStatusCode.Conflict,
-          body: {
-            error:
-              "O email informado já está sendo utilizado no cadastro do cliente " +
-              emailAlreadyUsedOnTheCustomer.firstname +
-              " " +
-              emailAlreadyUsedOnTheCustomer.lastname,
-          },
-        };
+      const isUsedEmail = await this.validateExistEmail(customer.email);
+      if (isUsedEmail) {
+        return isUsedEmail;
       }
-
-      const CPFAlreadyUsedOnTheCustomer = 
-      await this.repository.findByCPF(newCustomer.cpf);
-      if (CPFAlreadyUsedOnTheCustomer) {
-        return {
-          statusCode: HttpStatusCode.Conflict,
-          body: {
-            error:
-              "O CPF informado já está sendo utilizado no cadastro do cliente " +
-              CPFAlreadyUsedOnTheCustomer.firstname +
-              " " +
-              CPFAlreadyUsedOnTheCustomer.lastname,
-          },
-        };
+      
+      const isUsedCPF = await this.validateExistCPF(customer.cpf);
+      if (isUsedCPF) {
+        return isUsedCPF;
       }
-
-      const createdCustomer = 
-      await this.repository.create(newCustomer);
+      
+      const newCustomer = 
+      await this.repository.create(customer);
       return {
         statusCode: HttpStatusCode.Created,
-        body: createdCustomer,
+        body: newCustomer,
       };
-    } catch (error) {
+    } 
+    catch (error) {
+      console.log(error)
       return {
         statusCode: HttpStatusCode.InternalServerError,
         body: {
@@ -77,4 +55,51 @@ export class CreateCustomerUseCase implements ICreateCustomerUseCase {
       };
     }
   }
+
+  private async validateExistAccount(accountIdRef: string): Promise<IHttpResponse<IHttpErrorResponse> | null> {
+    const isExistAccount = 
+      await this.validateExistAccountService
+      .validate(accountIdRef);
+      if (!isExistAccount) {
+        return {
+          statusCode: HttpStatusCode.NotFound,
+          body: {
+            error: "Conta não encontrada! Faça o login ou se cadastre!",
+          },
+        };
+      }
+      return null
+  }
+
+  private async validateExistEmail(email: string): Promise<IHttpResponse<IHttpErrorResponse> | null> {
+    const customerAlreadUsesEmail = 
+    await this.repository.findByEmail(email);
+    if (customerAlreadUsesEmail){
+      const fullName =
+        customerAlreadUsesEmail.firstname + customerAlreadUsesEmail.lastname;
+      return {
+        statusCode: HttpStatusCode.Conflict,
+        body: {
+          error:
+            "O email informado já está sendo utilizado no cadastro do cliente " + fullName
+        },
+      };
+    }
+    return null
+  }
+
+  private async validateExistCPF(cpf: string): Promise<IHttpResponse<IHttpErrorResponse> | null> {
+    const customerAlreadUsesCPF = await this.repository.findByCPF(cpf);
+      
+    if (customerAlreadUsesCPF) {      
+        return {
+          statusCode: HttpStatusCode.Conflict,
+          body: {
+            error:
+              "O CPF informado já está sendo utilizado no cadastro do cliente " + customerAlreadUsesCPF.getFullName()
+          },
+        };
+      }
+      return null
+    }
 }

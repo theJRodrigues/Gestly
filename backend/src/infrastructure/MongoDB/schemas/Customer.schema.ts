c0d18291c42812d2587cd453f9c04d0c932afd0c
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-04-13 21:26:52 -0300

    refactor: remove minmax lenght in password.

--- backend/src/infrastructure/MongoDB/schemas/Customer.schema.ts
+++ backend/src/infrastructure/MongoDB/schemas/Customer.schema.ts
@@ -41,7 +41,6 @@ export const customerSchema = new Schema<CustomerDTO>(
         ret.id = ret._id;
         delete ret._id;
         delete ret.__v;
-        delete ret.accountIdRef;
       },
     },
   }
